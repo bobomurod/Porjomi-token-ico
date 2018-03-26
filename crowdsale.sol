a7626809e560ec6670926d2b4cc7885a5b6b3183
bobomurod <bobomurod.boboev@yandex.ru>
2018-03-26 06:50:05 +0300

    variables

--- crowdsale.sol
+++ crowdsale.sol
@@ -1,7 +1,12 @@
 pragma solidity ^0.4.18;
 
 contract Crowdsale() {
+    uint investment;
+    address investor;
+    address owner;
+
     function() {
-        
+        investor = msg.sender;
+        invesment = msg.value;
     }   
 }
